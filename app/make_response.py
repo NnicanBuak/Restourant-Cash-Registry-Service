18cad0011804ed43cc127a80c2ac2299c581b016
JartiX <artempilavin@gmail.com>
2024-11-06 02:58:17 +0800

    Добавление узлов API

--- app/make_response.py
+++ app/make_response.py
@@ -1,4 +1,4 @@
-import json
+from flask import jsonify
 
 
 def make_response(text):
@@ -7,25 +7,26 @@ def make_response(text):
     if isinstance(text, (str, int, float)):
         text = {"responce": text}
 
-    json_text = json.dumps(text, sort_keys=True, indent=4, ensure_ascii=False)
+    json_text = jsonify(text)
 
     # Подготовить ответ
     result = app.response_class(
-        response=f"{json_text}", status=200, mimetype="application/json; charset=utf-8"
+        response=f"{json_text.json}", status=200, mimetype="application/json; charset=utf-8"
     )
 
     return result
 
 
 def make_error(text, error: int):
+    from . import app
     if isinstance(text, (str, int, float)):
         text = {"error": text}
 
-    json_text = json.dumps(text, sort_keys=True, indent=4, ensure_ascii=False)
+    json_text = jsonify(text)
 
     # Подготовить ответ
     result = app.response_class(
-        response=f"{json_text}",
+        response=f"{json_text.json}",
         status=error,
         mimetype="application/json; charset=utf-8",
     )
