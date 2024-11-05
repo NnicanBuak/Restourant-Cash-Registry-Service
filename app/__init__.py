e2aa95344d057863bab3d8ace6e1b1f550fda44e
NnicanBuak <mybailengdb@gmail.com>
2024-11-05 21:26:58 +0800

    refactoring

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from .config import Config
 from flask_admin import Admin, BaseView, expose
-from flask_admin.contrib.sqla import ModelView
 from .models import db, models_list
 
 
--- run.py
+++ run.py
@@ -1,9 +1,4 @@
 from app import app
-from app.api_routes import api_bp
-from app.page_routes import page_bp
-
-app.register_blueprint(api_bp)
-app.register_blueprint(page_bp)
 
 if __name__ == "__main__":
     app.run(debug=False)
